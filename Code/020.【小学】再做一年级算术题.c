3de13f5f1e6fdf5e7f4564cbf3c84001087c62dd
SeeChen Lee <39422761+SeeChen@users.noreply.github.com>
2021-05-26 10:35:06 +0800

    Update 020.【小学】再做一年级算术题.c

--- Code/020.【小学】再做一年级算术题.c
+++ Code/020.【小学】再做一年级算术题.c
@@ -7,3 +7,25 @@
 提示：
     关键是在表达式输入过程中如何取出运算符？
 */
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	float d1,d2;
+	char op;
+	scanf("%f %c %f",&d1,&op,&d2);
+	switch( op){
+		case '+':
+			printf("%g + %g = %g\n",d1,d2,d1+d2);break;
+		case '-':
+			printf("%g - %g = %g\n",d1,d2,d1-d2);break;
+		case '*':
+			printf("%g * %g = %g\n",d1,d2,d1*d2);break;
+		case '/':
+			printf("%g / %g = %g\n",d1,d2,d1/d2);break;
+		case '%':
+			printf("%g %% %g = %d\n",d1,d2,(int) d1 % (int) d2);break;
+		default:
+			printf("unkown op\n");break;
+	}
+	return 0;
+}
