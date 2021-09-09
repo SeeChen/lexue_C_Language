75b58207c2552fe5989740dc384e9a8223c5d959
SeeChen Lee <39422761+SeeChen@users.noreply.github.com>
2021-09-09 21:41:57 +0800

    None

--- Code/001. 简单的printf和scanf.c
+++ Code/001. 简单的printf和scanf.c
@@ -6,7 +6,7 @@ printf函数和scanf函数是C语言中最常用的函数之一，下面让我�
     输出整型变量x对应的十进制、八进制和十六进制形式。
 认真看看 -1 的八进制和十六进制输出，请想想是为什么。
 */
-#include "stdio.h"     
+#include <stdio.h>    
 int main(){     
     int x;     
     scanf("%d",&x);     
